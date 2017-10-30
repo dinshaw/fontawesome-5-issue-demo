c60f8dd72d241308557e92215056212da2466390
Dinshaw <gobhai@gmai.com>
2017-10-30 19:49:46 -0400

    remove php code

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>FontawesomeCssContent</title>
+
+    <script src="jquery.js"></script>
+    <script src="app.js"></script>
+    <link rel="stylesheet" media="all" href="app.css" />
+  </head>
+
+  <body>
+    <div class='container'></div>
+    <div><i class='fa fa-cog'></i></div>
+    <div class="user">User</div>
+
+    <script src="everything.js"></script>
+  </body>
+</html>
+
